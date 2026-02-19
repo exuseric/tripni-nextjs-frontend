ecaf584ebf6181dc2ebcb4fbca872756041c2077
exuseric <ericbro29@gmail.com>
2026-02-19 18:30:46 +0300

    style(ui): update theme tokens and refactor buttons - Refactor button utilities. - Add `@primer/octicons-react` dependency.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,19 +1,23 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
     return (
         <>
             <header className="w-full h-full min-h-96 bg-surface-container flex-center-col">
                 <h1 className="text-3xl font-bold text-primary">Trace Your Tracks Across the 254</h1>
                 <p>Plan, Log, and Share Your Kenyan Odyssey</p>
-                <Link href="/auth/sign-in" className="btn-filled mt-4">Begin your journey</Link>
+                <Link href="/auth/sign-in" className="btn-filled mt-4">
+                    Begin your journey
+                </Link>
             </header>
 
             <section className="py-container-block">
                 <header className="container flex-center-col">
-                    <h2 className="text-center font-bold text-2xl max-w-prose">Whether you’re chasing the Great Migration across the Mara, navigating the vibrant streets of
-                        Nairobi, or discovering hidden gems in the Rift Valley, our platform is your digital
-                        compass.</h2>
+                    <h2 className="text-center font-bold text-2xl max-w-prose">
+                        Whether you’re chasing the Great Migration across the Mara, navigating the vibrant streets of
+                        Nairobi, or
+                        discovering hidden gems in the Rift Valley, our platform is your digital compass.
+                    </h2>
                 </header>
             </section>
         </>
