19b2ae3a941a9f37b37e88f35e851800c941e291
exuseric <ericbro29@gmail.com>
2026-03-06 12:41:36 +0300

    feat(feed): enhance mobile navigation and update styles

--- app/(feed)/components/navigation/FeedMobileNavigation.tsx
+++ app/(feed)/components/navigation/FeedMobileNavigation.tsx
@@ -1,17 +1,18 @@
-import {LINKS} from '@/common/constants/url.constant';
-import {Link} from '@/common/ui/Link';
-import {PlusCircleIcon} from '@phosphor-icons/react/dist/ssr';
-import {MobileMenu} from '@/app/(feed)/components/navigation/components/MobileMenu';
+import { LINKS } from '@/common/constants/url.constant';
+import { Link } from '@/common/ui/Link';
+import { PlusCircleIcon } from '@phosphor-icons/react/dist/ssr';
+import { MobileMenu } from '@/app/(feed)/components/navigation/components/MobileMenu';
+import { SmoothBlur } from '@/common/components/SmoothBlur';
 
 
 export const FeedMobileNavigation = () => {
     return (
-        <div className="bg-surface flex-col-center pb-safe-bottom fixed inset-x-0 bottom-0 z-50 h-fit">
+        <div className="bg-transparent flex-col-center pb-safe-bottom fixed inset-x-0 bottom-0 z-50 h-fit">
             <div className="container">
-                <div className="col-span-full flex-row-between justify-center items-center gap-x-2">
-                    <MobileMenu/>
+                <div className="col-span-full flex-row-between justify-center items-center gap-x-1">
+                    <MobileMenu />
                     <Link href={LINKS.PROTECTED.createTrip} variant="tonal" aria-label="Create a trip" isIconOnly>
-                        <PlusCircleIcon size={32} weight="fill"/>
+                        <PlusCircleIcon size={32} weight="fill" />
                     </Link>
                 </div>
             </div>
--- app/(feed)/components/navigation/components/MobileMenu.tsx
+++ app/(feed)/components/navigation/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
-import {Link} from '@/common/ui/Link';
-import {LINKS} from '@/common/constants/url.constant';
-import {BinocularsIcon, MagnifyingGlassIcon, PathIcon, UserCircleIcon, UserIcon} from '@phosphor-icons/react/dist/ssr';
+import { Link } from '@/common/ui/Link';
+import { LINKS } from '@/common/constants/url.constant';
+import { BinocularsIcon, MagnifyingGlassIcon, PathIcon, UserCircleIcon, UserIcon } from '@phosphor-icons/react/dist/ssr';
 
 
 const feedLinks = [
@@ -26,13 +26,13 @@ const feedLinks = [
         id: 3,
         label: "profile",
         url: LINKS.PROTECTED.profile,
-        icon:<UserCircleIcon size={20} weight="fill" />
+        icon: <UserCircleIcon size={20} weight="fill" />
     }
 ]
 
 export const MobileMenu = () => {
     return (
-        <nav className="link flex-row-between gap-x-0 rounded-full w-full h-full p-1 bg-surface-container-low ring ring-primary/10 ring-inset">
+        <nav className="link flex-row-between gap-x-1 rounded-full w-full h-full p-1 glass ring ring-primary/10 ring-inset">
             {feedLinks.map((link) => (
                 <Link href={link.url} className="capitalize text-xs font-bold flex-col-center py-1 px-1 gap-y-1" key={link.id} variant={link.id === 0 ? "tonal" : "quiet"}>
                     {link.icon}
